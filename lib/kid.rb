5e259045ed39c055de080c4233fa2fd204867885
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-02 15:13:55 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,9 +1,8 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-  extend MetaDancing
-end
   include Dance
+  extend MetaDancing
 
   attr_accessor :name
 
